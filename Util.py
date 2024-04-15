10ffbf291b3bac149dc81dfc9115ff41f0f4de55
adeel-shahid <adeelshahidkhan62@gmail.com>
2024-04-15 14:40:28 +0500

    Add Batch Logic Updated

--- Util.py
+++ Util.py
@@ -5,9 +5,8 @@ def get_formatted_number(type):
     current_datetime = datetime.now()
     date_str = current_datetime.strftime("%d-%m-%Y")  # Format: dd-mm-yyyy
     time_str = current_datetime.strftime("%H:%M:%S")  # Format: hh:mm:ss
-    result = type + '#' + date_str.replace('-', '') + time_str.replace(':', '')
+    result = str(type) + '#' + date_str.replace('-', '') + time_str.replace(':', '')
     return result
-
 
 def get_current_date():
     return datetime.now().strftime("%x")
@@ -15,4 +14,13 @@ def get_current_date():
 def get_first_three_characters(value):
     if len(value)>=3:
         return value[0:3]
+
+
+def convert_to_kg(weight, unit):
+    if unit == 'g':
+        return weight / 1000
+    elif unit == 'mg':
+        return weight / 1000000
+    else:
+        return weight
 
